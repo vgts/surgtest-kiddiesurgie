7a552544001d6439af23d5e2a0e2159df1002887
anu <ranu.tsy@gmail.com>
2025-11-19 18:04:14 +0530

    updated code

--- main.js
+++ main.js
@@ -5,6 +5,62 @@ const navbarLoaded = fetch("navbar.html")
     document.getElementById("navbar").innerHTML = html;
   });
 
+// Load Page1
+const page1Loaded = fetch("page1.html")
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("page1").innerHTML = html;
+  });
+
+// Load Page2
+const page2Loaded = fetch("page2.html")
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("page2").innerHTML = html;
+  });
+
+// Load Page3
+const page3Loaded = fetch("page3.html")
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("page3").innerHTML = html;
+  });
+
+// Load Page4
+const page4Loaded = fetch("page4.html")
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("page4").innerHTML = html;
+  });
+
+// Load Page5
+const page5Loaded = fetch("page5.html")
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("page5").innerHTML = html;
+  });
+
+  // Load Page6
+const page6Loaded = fetch("page6.html")
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("page6").innerHTML = html;
+  });
+
+// Load Page7
+const page7Loaded = fetch("page7.html")
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("page7").innerHTML = html;
+  });
+
+// Load Page8
+const page10Loaded = fetch("page8.html")
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("page8").innerHTML = html;
+  });
+
 // Load Mobile Menu
 const menuLoaded = fetch("mobileMenu.html")
   .then(res => res.text())
@@ -33,3 +89,4 @@ Promise.all([navbarLoaded, menuLoaded]).then(() => {
     mobileMenu.style.right = "-260px";
   });
 });
+
